3cba1c26b70f581d7ed961c5c43767a2358725a8
Brandon Demello <demellobrandon@gmail.com>
2019-04-10 16:00:48 -0500

    Clear after click

--- QTerminal/UI.cs
+++ QTerminal/UI.cs
@@ -17,27 +17,39 @@ namespace testDotNet
             //initialize the application
             Application.Init ();
             var top = Application.Top;
-
-            var win = new Window(new Rect(0,1,top.Frame.Width,top.Frame.Height-1),"Class Test Application");
+            Rect mainRect =  new Rect(0,1,top.Frame.Width,top.Frame.Height-1);
+            var win = new Window(mainRect,"Class Test Application");
             top.Add(win);
 
             bool studentPressed = false;
             bool teacherPressed = false;
             var student = new Button(7,14,"Student")
             {
-                Clicked = () => { new Window(new Rect(0,1,top.Frame.Width,top.Frame.Height-1),"test");  studentPressed = true; }
+                Clicked = () => {   studentPressed = true;  }
             };
             var teacher = new Button(7,14,"Teacher")
             {
-                Clicked = () => { win.Clear();  teacherPressed = true; }
+                Clicked = () => {  teacherPressed = true; }
             };
             var dialog = new Dialog("Selection", 60, 7, student, teacher);
             win.Add(dialog);
 
 
+            if(studentPressed)
+            {
+            win.Remove(dialog);
+            win.Add();
+
+
+            }
+
+            if(teacherPressed)
+            {
+            win.Remove(dialog);
+             win.Add();
+
+            }
              Application.Run ();
-             Console.WriteLine("Student"+studentPressed);
-             Console.WriteLine("Teacher"+teacherPressed);
         }
     }
 }
