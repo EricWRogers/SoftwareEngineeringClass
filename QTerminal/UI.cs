cbb88e56ba999049bb018901da9bba2f18e23819
Brandon Demello <demellobrandon@gmail.com>
2019-04-10 16:22:19 -0500

    fixed up UI some more better looking and not repetitive

--- QTerminal/UI.cs
+++ QTerminal/UI.cs
@@ -20,36 +20,22 @@ namespace testDotNet
             Rect mainRect =  new Rect(0,1,top.Frame.Width,top.Frame.Height-1);
             var win = new Window(mainRect,"Class Test Application");
             top.Add(win);
-
-            bool studentPressed = false;
-            bool teacherPressed = false;
-            var student = new Button(7,14,"Student")
-            {
-                Clicked = () => {   studentPressed = true; }
-            };
-            var teacher = new Button(7,14,"Teacher")
-            {
-                Clicked = () => {  teacherPressed = true; }
-            };
+            
+            var student = new Button(7,14,"Student");
+            var teacher = new Button(7,14,"Teacher");
             var dialog = new Dialog("Selection", 60, 7, student, teacher);
+
             win.Add(dialog);
             //var testView = new ListView(new Rect(4, 8, top.Frame.Width, 200), MovieDataSource.GetList(forKidsOnly.Checked, 0).ToList());
-            //var testView = new ListView(new Rect(4, 8, top.Frame.Width, 200), MovieDataSource.GetList(forKidsOnly.Checked, 0).ToList());
+             student.Clicked = () =>
+             {
+                win.Remove(dialog);
+                win.Redraw(mainRect);
+                //win.Add(testView);
 
 
-            if(studentPressed)
-            {
-            win.Remove(dialog);
-            win.Redraw(mainRect);
-            //win.Add();
-            }
-
-            if(teacherPressed)
-            {
-            win.Remove(dialog);
-            win.Redraw(mainRect);
-            //win.Add();
-            }
+             };
+        
              Application.Run ();
         }
     }
