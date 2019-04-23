6e47d026c702a06ab8fcc7042aa73c63a1d1d441
Justin Liles <Justin.Liles@Live.com>
2019-04-23 14:00:30 -0500

    Updated Unit Testing

--- QTerminal.Tests/ModelTests.cs
+++ QTerminal.Tests/ModelTests.cs
@@ -11,13 +11,51 @@ namespace QTerminal.Tests
         [Theory]
         [InlineData("../../../../QTerminal/Test", "test")]
         [InlineData("../../../../QTerminal/StudentAnswer", "testAnswer")]
-        [InlineData("kjhkjh", "kjhkjh")]
         public void LoadAvailableTest_TestingForErrors(string dir, string fileEX)
         {
             String[] expected = {"Error"};
             Assert.NotEqual<String>(expected, Model.LoadAvailableTest(dir, fileEX));
         }
 
-        
+        [Theory]
+        [InlineData("../../../../QTerminal/StudentAnswer","Justin", "testAnswer")]
+        [InlineData("../../../../QTerminal/StudentAnswer","Test", "testAnswer")]
+        [InlineData("../../../../QTerminal/StudentAnswer","UnitTest", "testAnswer")]
+        public void SaveTest(string dir,string fileName, string fileEx)
+        {
+            Test test = new Test();
+            string path = dir + "/" + fileName + "." + fileEx;
+
+            Model.SaveTest(test, dir, fileName, fileEx);
+            Assert.True(System.IO.File.Exists(path));
+            System.IO.File.Delete(path);
+        }
+
+        [Theory]
+        [InlineData("Justin")]
+        [InlineData("UnitTesting")]
+        [InlineData("Test")]
+        public void makeFolder(string FolderName)
+        {
+            Model.makeFolder(FolderName);
+
+            string path = "./" + FolderName;
+            
+            Assert.True(System.IO.Directory.Exists(path));
+            System.IO.Directory.Delete(path);
+        }
+
+        [Theory]
+        [InlineData("Justin", "UnitTest", ".TEST", "Justin")]
+        public void LoadTestNames(string testName, string dirName, string extension, string excepted)
+        {
+            Test test = new Test();
+            Model.SaveTest(test, dirName, testName, extension);
+
+            string[] testFileNames = {testName};
+            string[] exceptedOutcomes = {excepted};
+
+            Assert.Equal(exceptedOutcomes, Model.LoadTestNames(testFileNames, dirName, extension));
+        }
     }
 }
